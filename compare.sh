a7c84c3f4a8f8f7ff5abf8d7353410d6497e68b6
Paul Swartz <paul@paulswartz.net>
2018-01-25 15:45:33 -0500

    wip

--- compare.sh
+++ compare.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -e
+#wget -O /tmp/VehiclePositions.pb "https://s3.amazonaws.com/mbta-gtfs-s3-dev-green/concentrate/VehiclePositions.pb"
+#wget -O /tmp/TripUpdates.pb "https://s3.amazonaws.com/mbta-gtfs-s3-dev/concentrate/TripUpdates.pb"
 #wget -O /tmp/vp.pb "http://developer.mbta.com/lib/GTRTFS/Alerts/VehiclePositions.pb"
 #wget -O /tmp/tu.pb "http://developer.mbta.com/lib/GTRTFS/Alerts/TripUpdates.pb"
-#wget -O /tmp/tu.pb "https://s3.amazonaws.com/mbta-realtime-prod/tripupdates.pb"
-wget -O /tmp/vp.pb "https://s3.amazonaws.com/mbta-realtime-prod/vehiclepositions.pb"
+wget -q -O /tmp/tu.pb "https://s3.amazonaws.com/mbta-realtime-prod/tripupdates.pb"
+#wget -O /tmp/vp.pb "https://s3.amazonaws.com/mbta-realtime-prod/vehiclepositions.pb"
 
-wget -O /tmp/VehiclePositions.pb "https://s3.amazonaws.com/mbta-gtfs-s3/concentrate/VehiclePositions.pb"
-
-mix diff_pb /tmp/vp.pb /tmp/VehiclePositions.pb
+#mix diff_pb /tmp/vp.pb /tmp/VehiclePositions.pb
+mix diff_pb /tmp/tu.pb /tmp/TripUpdates.pb
--- lib/mix/tasks/diff_pb.ex
+++ lib/mix/tasks/diff_pb.ex
@@ -8,7 +8,15 @@ defmodule Mix.Tasks.DiffPb do
     GTFSRealtimeViz.new_message(:second, File.read!(second), second)
     first_vehicles = vehicles(:first)
     second_vehicles = vehicles(:second)
-    IO.inspect(diff_pairs(first_vehicles, second_vehicles, &vehicle_sort_key/1, &vehicle_diff/3))
+    first_trip_updates = trip_updates(:first)
+    second_trip_updates = trip_updates(:second)
+    for diff <- diff_pairs(first_vehicles, second_vehicles, &vehicle_sort_key/1, &vehicle_diff/3) do
+      IO.inspect(diff)
+    end
+    for diff <- diff_pairs(first_trip_updates, second_trip_updates, &trip_update_sort_key/1, &trip_update_diff/3),
+      elem(diff, 0) == :changed do
+      IO.inspect(diff)
+    end
   end
 
   def vehicles(group) do
@@ -16,19 +24,29 @@ defmodule Mix.Tasks.DiffPb do
     Enum.sort_by(vehicles, &vehicle_sort_key/1)
   end
 
-  def vehicle_sort_key(%{vehicle: %{id: id}}), do: id
-  def vehicle_sort_key(_), do: nil
-
-  def diff_pairs(first, [], _, _) do
-    for item <- first do
-      {:removed, first}
-    end
+  def trip_updates(group) do
+    [{_, trip_updates}] = State.trip_updates(group)
+    Enum.sort_by(trip_updates, &trip_update_sort_key/1)
   end
 
-  def diff_pairs([], second, _, _) do
-    for item <- second do
-      {:added, second}
+  def vehicle_sort_key(%{vehicle: %{id: id}, trip: trip}), do: {id, trip.route_id, trip.direction_id}
+  def vehicle_sort_key(_), do: nil
+
+  def trip_update_sort_key(%{trip: %{} = trip}), do: {trip.trip_id, trip.route_id, trip.direction_id}
+  def trip_update_sort_key(_), do: nil
+
+  def diff_pairs(first, [], key_fn, _) do
+    for item <- first do
+      {:removed, key_fn.(item)}
     end
+    []
+  end
+
+  def diff_pairs([], second, key_fn, _) do
+    for item <- second do
+      {:added, key_fn.(item)}
+    end
+    []
   end
 
   def diff_pairs([first | rest_first], [second | rest_second], key_fn, diff_fn) do
@@ -37,19 +55,64 @@ defmodule Mix.Tasks.DiffPb do
     cond do
       first_key == second_key ->
         diff_fn.(first_key, first, second) ++ diff_pairs(rest_first, rest_second, key_fn, diff_fn)
-      first_key <= second_key ->
-        [{:removed, first}] ++ diff_pairs(rest_second, [second | rest_second], key_fn, diff_fn)
-      true ->
-        [{:added, second}] ++ diff_pairs([first | rest_first], rest_second, key_fn, diff_fn)
+      is_nil(first_key) ->
+        [{:no_first_key, first}] ++ diff_pairs(rest_first, [second | rest_second], key_fn, diff_fn)
+      is_nil(second_key) ->
+        [{:no_second_key, second}] ++ diff_pairs([first | rest_first], rest_second, key_fn, diff_fn)
+      first_key < second_key ->
+        #[{:removed, first_key}] ++
+        diff_pairs(rest_first, [second | rest_second], key_fn, diff_fn)
+      first_key > second_key ->
+        #[{:added, second_key}] ++
+          diff_pairs([first | rest_first], rest_second, key_fn, diff_fn)
     end
   end
 
   def vehicle_diff(key, first, second) do
-    for key <- [[:current_status], [:current_stop_sequence], [:trip, :trip_id]],
-      first_val = Access.get(first, key),
-      second_val = Access.get(second, key),
-      first_val != second_val do
-        {:changed, key, first_val, second_val}
+    diff_functions(key, first, second, [trip_id: & &1.trip.trip_id, stop_sequence: & &1.current_stop_sequence])
+  end
+
+  def trip_update_diff(key, _first, %{stop_time_update: []}) do
+    [{:changed, key, :removed_stop_updates}]
+  end
+  def trip_update_diff(key, %{stop_time_update: []}, _second) do
+    [{:changed, key, :added_stop_updates}]
+  end
+  def trip_update_diff(key, first, second) do
+    tu_diffs = diff_functions(key, first, second, [relationship: & &1.trip.schedule_relationship])
+    first = Enum.uniq(Enum.sort_by(first.stop_time_update, & &1.stop_sequence))
+    second = Enum.sort_by(second.stop_time_update, & &1.stop_sequence)
+    stu_diffs = for diff <- diff_pairs(first, second, & &1.stop_sequence, &stop_sequence_diff/3) do
+      {:changed, key, diff}
     end
+    tu_diffs ++ stu_diffs
+  end
+
+  def stop_sequence_diff(key, first, second) do
+    diff_functions(key, first, second, [relationship: & &1.schedule_relationship, has_arrival?: &not is_nil(&1.arrival), has_departure?: &not is_nil(&1.departure)])
+  end
+
+  def diff_functions(key, first, second, functions) do
+    for {name, val_fn} <- functions,
+      first_val = val_fn.(first),
+      second_val = val_fn.(second),
+      not valid?(name, first_val, second_val) do
+        {:changed, key, name, first_val, second_val}
+    end
+  end
+
+  defp valid?(:relationship, first_val, second_val) do
+    first_val = first_val || :SCHEDULED
+    second_val = second_val || :SCHEDULED
+    first_val == second_val
+  end
+  defp valid?(:stop_sequence, first_val, second_val) when is_integer(second_val) and first_val <= second_val do
+    true
+  end
+  defp valid?(_, same, same) do
+    true
+  end
+  defp valid?(_, _, _) do
+    false
   end
 end
